ca3f491280aec5cde771d7d6e29c469f60b166ef
MD Siam Sheikh <siamsabbir307@gmail.com>
2025-12-18 15:51:16 +0600

    (Fix) add something

--- app/ui/sidebar/NavLinks.tsx
+++ app/ui/sidebar/NavLinks.tsx
@@ -27,7 +27,7 @@ export default function NavLinks() {
       {links.map((link:{name: string, href: string, icon: React.ComponentType<React.SVGProps<SVGSVGElement>>}) => {
         const Icon: React.ComponentType<React.SVGProps<SVGSVGElement>> = link.icon;
         return(
-          <Link key={link.name} href={link.href} className='flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-purple-600 md:flex-none md:justify-start md:p-2 md:px-3'>
+          <Link key={link.name} href={link.href} className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-purple-600 md:flex-none md:justify-start md:p-2 md:px-3 ${link.name === 'Cart' && cartData.length <= 0 && 'hidden'}`}>
             <Icon className={`h-6 w-6 ${link.name === 'Cart' && cartData.length > 0 && 'text-purple-600'}`} />
             {/* <span className=' max-md:hidden'>{link.name === 'Cart' && cartData.length > 0 ? `${link.name} (${cartData.length})` : link.name}</span> */}
             {
